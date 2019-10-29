740058788cd214d6e71e8b4cd8189898573c93e4
AdmREQ <adamdowson@googlemail.com>
2019-10-29 10:11:41 +0000

    fix: added ID's back in for the PDF generation

--- src/assets/js/receipt.js
+++ src/assets/js/receipt.js
@@ -112,7 +112,9 @@ $('#download-receipt').click(function () {
     centeredText(amount, amountValueY);
 
     doc.setFontSize(14);
-    centeredText(amountUSD, amountUsdValueY);
+    if (amountUSD) {
+        centeredText(amountUSD, amountUsdValueY);
+    }
 
     doc.save('RequestInvoice');
 
