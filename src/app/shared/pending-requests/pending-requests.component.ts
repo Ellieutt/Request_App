4609695d8e4b72605e13a2b5ffd2fb99f994d045
AdmREQ <adamdowson@googlemail.com>
2019-11-04 16:06:47 +0000

    fix: revert pending-requests.component

--- src/app/shared/pending-requests/pending-requests.component.ts
+++ src/app/shared/pending-requests/pending-requests.component.ts
@@ -56,7 +56,6 @@ export class PendingRequestsComponent implements OnInit {
       this.requestList = newCookieList;
       if (newCookieList.length === 0) {
         this.broadcastingRequestCount = 0;
-        this.hasBeenClicked = true;	
         this.showPendingPopup = false;
       }
     }
@@ -93,6 +92,7 @@ export class PendingRequestsComponent implements OnInit {
       });
       this.cookieService.set('processing_requests', JSON.stringify(updatedCookieList), 1);
     }
+    this.hasBeenClicked = true;
     this.showPendingPopup = !this.showPendingPopup;
   }
 
