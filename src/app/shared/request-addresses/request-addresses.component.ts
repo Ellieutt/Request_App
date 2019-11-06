13578cf21fbc244bb10e5adf629bcce091326ace
AdmREQ <adamdowson@googlemail.com>
2019-11-06 11:25:00 +0000

    fix: address labels are different on the dashboard vs the request

--- src/app/shared/request-addresses/request-addresses.component.ts
+++ src/app/shared/request-addresses/request-addresses.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class RequestAddressesComponent implements OnInit {
   @Input()
   request: any;
+  @Input()
+  account: string;
   payer: string;
   currency: string;
   max: number = 0;
