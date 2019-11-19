33b2f8040d9e3298c324331a5b9a94db3ca1b858
Yo <56731761+yomarion@users.noreply.github.com>
2019-11-19 10:02:12 +0100

    fix:Compilation issue about removed filters (#72)
    * First UI elements and rough logic
    
    * refactor: Simplify pre-pagination for performance
    
    * feat:Financial filters
    
    * feat:Filters work well even with long loading and playing with pagination, but some optimizations can probably be done.
    
    * feat: Improved code & perf
    
    * style: cleaned comments
    
    * fix: A few UI improvements around the filters and column sizing
    
    * feat:Few fixes of UI
    
    * clean:Removed useless comments, console logs and 1 useless div
    
    * fix: Compilation did not pass
    
    * fix:pageEvent required
    
    * fix: removing sorting options that have not worked for a while
    
    * fix:Compilation issue

--- src/app/invoices/search/search.component.ts
+++ src/app/invoices/search/search.component.ts
@@ -147,7 +147,7 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
 
         this.handlePageLoading();
         this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
+        //this.dataSource.sort = this.sort;
 
         this.loading = false;
         this.updateAndShowPendingRequests();
@@ -280,7 +280,7 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    //this.dataSource.sort = this.sort;
   }
 
   ngOnDestroy() {
