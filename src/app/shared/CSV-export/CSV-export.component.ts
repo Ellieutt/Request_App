b6361e9a7628e8bdac53abcbd58daad777d3fede
yomarion <56731761+yomarion@users.noreply.github.com>
2019-11-29 16:23:09 +0100

    Cleaned code and file name.

--- src/app/shared/CSV-export/CSV-export.component.ts
+++ src/app/shared/CSV-export/CSV-export.component.ts
@@ -23,50 +23,11 @@ export class CSVExportComponent {
         if (this.nbValid() == this.requests.length) {
             this.exportToCsv();
         } else {
-            console.log(">" + this.nbValid() + "-" + this.requests.length);
             this.togglePopup();
         }
     }
 
     togglePopup() {
-        // TEST partial requests
-        //this.requests = JSON.parse('[{"_meta":{"blockNumber":8528181,"timestamp":1568200748},"requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004c1"},{"_meta":{"blockNumber":8524618,"timestamp":1568152619},"requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004b3"},{"_meta":{"blockNumber":8520809,"timestamp":1568100828},"requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004af"},{"_meta":{"blockNumber":8495285,"timestamp":1567757688},"requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004ac"}]');
-        // TEST completed requests
-        //this.requests = JSON.parse('[{"_meta":{"blockNumber":8528181,"timestamp":1568200748},"requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004c1","request":{"creator":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","currencyContract":{"payeePaymentAddress":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","subPayeesPaymentAddress":[],"address":"0x3038045cd883aBff0C6eEa4B1954843c0fA5a735"},"data":{"hash":"QmQEFJ3EH1a96Kx9zKTbZdSwP9F6NSF7XFtqikjSVQrgvA","data":{"date":1568193834921,"reason":"Is it Rinkeby or the round table or bad luck?","miscellaneous":{"builderId":"app.request.network"}}},"payee":{"address":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","balance":"0","expectedAmount":"2386f26fc10000","label":"Benji"},"payer":"0x8400b234e7B113686bD584af9b1041E5a233E754","requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004c1","state":0,"subPayees":[],"currency":"ETH","status":"created","payerLabel":"Yoyo"}},{"_meta":{"blockNumber":8524618,"timestamp":1568152619},"requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004b3","request":{"creator":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","currencyContract":{"tokenAddress":"0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359","payeePaymentAddress":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","subPayeesPaymentAddress":[],"address":"0x3baA64a4401Bbe18865547E916A9bE8e6dD89a5A"},"data":{"hash":"QmdUQ8834GYLZMGtSGkp8e95Arct7gp8atdGBxcLg8ewpJ","data":{"date":1568116346798,"reason":"A test for Bernardo","miscellaneous":{"builderId":"app.request.network"}}},"payee":{"address":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","balance":"0","expectedAmount":"de0b6b3a7640000","label":"Benji"},"payer":"0x8400b234e7B113686bD584af9b1041E5a233E754","requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004b3","state":0,"subPayees":[],"currency":"DAI","status":"created","payerLabel":"Yoyo"}},{"_meta":{"blockNumber":8520809,"timestamp":1568100828},"requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004af","request":{"creator":"0x8400b234e7B113686bD584af9b1041E5a233E754","currencyContract":{"payeePaymentAddress":"0x8400b234e7B113686bD584af9b1041E5a233E754","subPayeesPaymentAddress":[],"address":"0x3038045cd883aBff0C6eEa4B1954843c0fA5a735"},"data":{"hash":"QmYJ8NkUnAf61BSoEQdRh59JTnAzgtkjzRP4kAMsgfankB","data":{"date":1568098265633,"reason":"A reason for Vincent","miscellaneous":{"builderId":"app.request.network"}}},"payee":{"address":"0x8400b234e7B113686bD584af9b1041E5a233E754","balance":"0","expectedAmount":"11c37937e08000","label":"Yoyo"},"payer":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004af","state":0,"subPayees":[],"currency":"ETH","status":"created","payerLabel":"Benji"}},{"_meta":{"blockNumber":8495285,"timestamp":1567757688},"requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004ac","request":{"creator":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","currencyContract":{"payeePaymentAddress":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","subPayeesPaymentAddress":[],"address":"0x3038045cd883aBff0C6eEa4B1954843c0fA5a735"},"data":{"hash":"QmexPvEHTPeDRvkHAErZNJ4bV5DNSADT7xTCJ2iVsxpgHi","data":{"date":1567757563404,"reason":"Test expenses Aug-2019","miscellaneous":{"builderId":"app.request.network"}}},"payee":{"address":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","balance":"2386f26fc10000","expectedAmount":"2386f26fc10000","label":"Benji"},"payer":"0x8400b234e7B113686bD584af9b1041E5a233E754","requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004ac","state":1,"subPayees":[],"currency":"ETH","status":"paid","payerLabel":"Yoyo"}}]');
-        /*this.requests = JSON.parse('[
-            {"_meta":{"blockNumber":8528181,"timestamp":1568200748},
-            "requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004c1",
-            "request":{
-                "creator":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3",
-                "currencyContract":{
-                    "payeePaymentAddress":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","subPayeesPaymentAddress":[],"address":"0x3038045cd883aBff0C6eEa4B1954843c0fA5a735"},
-                "data":{
-                    "hash":"QmQEFJ3EH1a96Kx9zKTbZdSwP9F6NSF7XFtqikjSVQrgvA",
-                    "data":{"date":1568193834921,
-                        "reason":"Is it Rinkeby or the round table or bad luck?",
-                        "miscellaneous":{"builderId":"app.request.network"}
-                    }
-                },"payee":{
-                    "address":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3",
-                    "balance":"0",
-                    "expectedAmount":"2386f26fc10000",
-                    "label":"Benji"
-                },
-                "payer":"0x8400b234e7B113686bD584af9b1041E5a233E754",
-                "requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004c1",
-                "state":0,
-                "subPayees":[],
-                "currency":"ETH",
-                "status":"created",
-                "payerLabel":"Yoyo"}
-            },
-            {"_meta":{
-                "blockNumber":8524618,
-                "timestamp":1568152619
-            },
-            "requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004b3",
-            "request":{"creator":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","currencyContract":{"tokenAddress":"0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359","payeePaymentAddress":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","subPayeesPaymentAddress":[],"address":"0x3baA64a4401Bbe18865547E916A9bE8e6dD89a5A"},"data":{"hash":"QmdUQ8834GYLZMGtSGkp8e95Arct7gp8atdGBxcLg8ewpJ","data":{"date":1568116346798,"reason":"A test for Bernardo","miscellaneous":{"builderId":"app.request.network"}}},"payee":{"address":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","balance":"0","expectedAmount":"de0b6b3a7640000","label":"Benji"},"payer":"0x8400b234e7B113686bD584af9b1041E5a233E754","requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004b3","state":0,"subPayees":[],"currency":"DAI","status":"created","payerLabel":"Yoyo"}},{"_meta":{"blockNumber":8520809,"timestamp":1568100828},"requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004af","request":{"creator":"0x8400b234e7B113686bD584af9b1041E5a233E754","currencyContract":{"payeePaymentAddress":"0x8400b234e7B113686bD584af9b1041E5a233E754","subPayeesPaymentAddress":[],"address":"0x3038045cd883aBff0C6eEa4B1954843c0fA5a735"},"data":{"hash":"QmYJ8NkUnAf61BSoEQdRh59JTnAzgtkjzRP4kAMsgfankB","data":{"date":1568098265633,"reason":"A reason for Vincent","miscellaneous":{"builderId":"app.request.network"}}},"payee":{"address":"0x8400b234e7B113686bD584af9b1041E5a233E754","balance":"0","expectedAmount":"11c37937e08000","label":"Yoyo"},"payer":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004af","state":0,"subPayees":[],"currency":"ETH","status":"created","payerLabel":"Benji"}},{"_meta":{"blockNumber":8495285,"timestamp":1567757688},"requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004ac","request":{"creator":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","currencyContract":{"payeePaymentAddress":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","subPayeesPaymentAddress":[],"address":"0x3038045cd883aBff0C6eEa4B1954843c0fA5a735"},"data":{"hash":"QmexPvEHTPeDRvkHAErZNJ4bV5DNSADT7xTCJ2iVsxpgHi","data":{"date":1567757563404,"reason":"Test expenses Aug-2019","miscellaneous":{"builderId":"app.request.network"}}},"payee":{"address":"0xAa0c45D2877373ad1AB2aa5Eab15563301e9b7b3","balance":"2386f26fc10000","expectedAmount":"2386f26fc10000","label":"Benji"},"payer":"0x8400b234e7B113686bD584af9b1041E5a233E754","requestId":"0xdb600fda54568a35b78565b5257125bebc51eb270000000000000000000004ac","state":1,"subPayees":[],"currency":"ETH","status":"paid","payerLabel":"Yoyo"}}]');
-                */
         this.popupVisible = !this.popupVisible;
     }
 
@@ -79,7 +40,16 @@ export class CSVExportComponent {
     }
 
     exportToCsv() {
-        const filename = 'requests.csv';
+        const today = new Date();
+        const filename = '' + 
+            today.getFullYear() + 
+            (today.getMonth() + 1).toString().padStart(2, '0') +
+            today.getDate().toString().padStart(2, '0') +
+            '_' +
+            today.getHours().toString().padStart(2, '0') +
+            today.getMinutes().toString().padStart(2, '0') +
+            today.getSeconds().toString().padStart(2, '0') +
+            '_requests.csv';
         var processRow = function (row) {
             var finalVal = '';
             for (var j = 0; j < row.length; j++) {
@@ -114,9 +84,6 @@ export class CSVExportComponent {
             "Block Number"
         ]);
         this.requests.filter(r => this.isValid(r)).forEach( req => {
-            if (req.requestId == "0x8fc2e7f2498f1d06461ee2d547002611b801202b000000000000000000000c67") {
-                console.log(req);
-            }
             rows.push([new Date (req._meta.timestamp * 1000),
                 req.requestId,
                 req.request.creator,
